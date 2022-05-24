290333950e0d7dd6ee96efd8f7a867c68430d91e
DEV-MAR2022\harri <powercommand2022@gmail.com>
2022-05-24 23:38:42 +0200

    Example layout

---
+++ src/3.1/Klingsten.CVScoringSystem/Klingsten.CVscoringSystem.WebClient/ClientConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace Klingsten.CVscoringSystem.WebClient;
+public class ClientConfiguration
+{
+    public const string Name = "Klingsten CVscoring System";
+    public const string Version = "0.0.1";
+}
