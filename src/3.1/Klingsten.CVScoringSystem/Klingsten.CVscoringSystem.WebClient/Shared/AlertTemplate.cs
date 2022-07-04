ca26a42440d1be44d5a34ba2159b2734711b00cd
DEV-MAR2022\harri <powercommand2022@gmail.com>
2022-07-04 21:34:39 +0200

    Minor changes

---
+++ src/3.1/Klingsten.CVScoringSystem/Klingsten.CVscoringSystem.WebClient/Shared/AlertTemplate.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor;
+
+namespace Klingsten.CVscoringSystem.WebClient.Shared;
+
+public class AlertTemplate
+{
+    public Severity Severity { get; set; }
+    public bool Show { get; set; }
+    public string Message { get; set; } = "";
+    public bool HasError { get; set; }
+}
